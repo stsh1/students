e72b0a0fc452eb9da528120c9737971ccfd89072
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-24 16:24:16 +0530

    Update view.php

--- view.php
+++ view.php
@@ -36,10 +36,10 @@
 				
 				$i=1;
 				while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-					$u_id = $row[0];
-					$user_name = $row[1];
-					$father_name=$row[2];
-					$roll_no=$row[4];
+					//$u_id = $row[0];
+					$user_name = $row[0];
+					$father_name=$row[1];
+					$roll_no=$row[2];
 					
 				
 			?>
