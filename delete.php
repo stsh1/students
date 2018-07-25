34f2df5842d5f40f9fbf46a4c072a03862f5f0f7
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 10:27:33 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ $delete_record = $_GET['del'];
 
 $query ="delete from u_reg where rollno='$delete_record'";
 
-if (mysql_query($query)){
+if (mysqlsrv_query($query)){
 
 	echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!');</script>,'_self')";
 }
