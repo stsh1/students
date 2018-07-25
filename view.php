db627946b6a075047964b9f454685cff14eaca11
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 09:56:40 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,21 +1,30 @@
 <?php
 			
-				include('config.php');
-				$tsql="SELECT * FROM u_reg";
-				$getResults= sqlsrv_query($conn, $tsql);
+<?php
+$serverName = "studentserv.database.windows.net";
+$connectionOptions = array(
+    "Database" => "studentsdb",
+    "Uid" => "ravdeep",
+    "PWD" => "Like@1234"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql="SELECT * FROM dbo.u_reg";
+
+$getResults= sqlsrv_query($conn, $tsql);
+
+echo ("Reading data from table" . PHP_EOL);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
+{
+ echo ($row['id'] . " " . $row['username']. $row['fname'] . $row['rollno']. PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);
+?>
+
+
+
+
+
 				
-	
-				
-				
-				$i=1;
-				while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-					//$u_id = $row[0];
-					$user_name = $row[0];
-					$father_name=$row[1];
-					$roll_no=$row[2];
-					
-					echo $user_name;
-					echo $father_name;
-					echo $roll_no;
-				}
-			?>
