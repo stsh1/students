b979dcfc469a70d61f26f23b8c891b311f14c908
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 10:00:09 +0530

    Update view.php

--- view.php
+++ view.php
@@ -9,18 +9,19 @@ $connectionOptions = array(
 );
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql="SELECT * FROM dbo.u_reg";
+$sqlselect="SELECT * FROM u_reg";
 
-$getResults= sqlsrv_query($conn, $tsql);
+$getResults= sqlsrv_query($conn, $sqlselect);
 
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
+
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
 {
- echo ($row['id'] . " " . $row['username']. $row['fname'] . $row['rollno']. PHP_EOL);
+ echo $row['username'];
 }
-sqlsrv_free_stmt($getResults);
+
 ?>
 
 
