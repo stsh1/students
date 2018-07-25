648dd04346c26caec428fac6b176a26b2d095f29
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 10:12:15 +0530

    Update view.php

--- view.php
+++ view.php
@@ -13,17 +13,7 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
  echo ($row['id'] . " " . $row['username'] . PHP_EOL);
 }
 ?>
-/*
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql="SELECT * FROM dbo.users";
-$getResults= sqlsrv_query($conn, $tsql);
-echo ("Reading data from table" . PHP_EOL);
-if ($getResults == FALSE)
-    echo (sqlsrv_errors());
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
- echo ($row['id'] . " " . $row['name'] . PHP_EOL);
-}
-sqlsrv_free_stmt($getResults);*/
+
 
 
 
