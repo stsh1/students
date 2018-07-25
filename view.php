f541fee5b27fc05e53bb0a190b6f82f419f25002
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 09:47:36 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,31 +1,4 @@
-<html>
-	<head><title>Viewing the Record</title>
-	<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-	<div class="container">
-	<?php include 'header.php'?>
-		<center><font color='red' size='6'><?php echo @$_GET['delete'];?>
-		<?php /*echo @$_GET['updated']; ?><?php echo @$_GET['logged']; */?>
-			</font></center>
-		<a href="logout.php">Logout</a>
-		<br></br><table align="center" width='900' border='4'>
-			<tr>
-			
-					<th colspan="20" align="center" bgcolor="yellow"><h1>Viewing all the record</h1></th>
-				
-			</tr>
-			<tr align="center">
-				<td>Serial No.</td>
-				<td>Student's Name</td>
-				<td>Father's Name</td>
-				<td>Roll No.</td>
-				<td>Delete</td>
-				<td>Edit</td>
-				<td>Detail</td>
-			</tr>
-	
-			<?php
+<?php
 			
 				include('config.php');
 				$tsql="SELECT * FROM u_reg";
@@ -41,17 +14,8 @@
 					$father_name=$row[1];
 					$roll_no=$row[2];
 					
+					echo $user_name;
+					echo $father_name;
+					echo $roll_no;
 				
 			?>
-				<tr align="center">
-				<td><?php echo $i;$i++; ?></td>
-				<td><?php echo $user_name; ?></td>
-				<td><?php echo $father_name; ?></td>
-				<td><?php echo $roll_no; ?></td>
-					</tr>
-			<?php } ?>
-		</table>
-		<?php include 'footer.php'?>
-		<div>
-	</body>
-</html>
