08cf85bd7504eed076d0ee2aba7e2a8e9f87b9f4
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 10:50:42 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $query ="DELETE FROM dbo.u_reg WHERE rollno='$delete_record'";
 
 if (sqlsrv_query($conn, $query)){
 
-	echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!');</script>,'_self')";
+	echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!'),'_self')<script>";
 }
 
 ?>
