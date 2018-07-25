8835a485039d7581a338f04d676d9ceae0a96f06
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-25 10:03:44 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,14 +1,7 @@
 <?php
 			
 <?php
-$serverName = "studentserv.database.windows.net";
-$connectionOptions = array(
-    "Database" => "studentsdb",
-    "Uid" => "ravdeep",
-    "PWD" => "Like@1234"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+include('config.php');
 $sqlselect="SELECT * FROM u_reg";
 
 $getResults= sqlsrv_query($conn, $sqlselect);
@@ -17,12 +10,12 @@ echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 
-while ($row = sqlsrv_fetch_assoc($getResults, SQLSRV_FETCH_ASSOC)) 
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
 {
- echo $row['fname'];
-	 echo $row['id'];
-	 echo $row['username'];
-	 echo $row['rollno'];
+ echo $row[0];
+	 echo $row[1];
+	 echo $row[2];
+	 echo $row[3];
 }
 
 ?>
