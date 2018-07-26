d1409b5ae7a4d0537bbdda6f1c98249f60dfa400
stsh1 <40880676+stsh1@users.noreply.github.com>
2018-07-26 09:44:55 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,9 +5,11 @@ $delete_record = $_GET['del'];
 
 $query ="DELETE FROM dbo.u_reg WHERE rollno='$delete_record'";
 
-if(sqlsrv_query($conn, $query)==true ){
+header("Location: view.php");
+
+/*if(sqlsrv_query($conn, $query)==true ){
 
 	echo "<script>window.open('view.php?delete=Record Has been Deleted Successfully!','_self')<script>";
-}
+}*/
 
 ?>
